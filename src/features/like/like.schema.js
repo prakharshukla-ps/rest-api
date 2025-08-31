4c384745d30b358f12e5cdfea82bdf29286c7947
prakharshukla-ps <pshukla650@gmail.com>
2025-08-31 10:25:20 +0530

    feat!: added pre & post methods

--- src/features/like/like.schema.js
+++ src/features/like/like.schema.js
@@ -4,4 +4,20 @@ export const LikeSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   likeable: { type: mongoose.Schema.Types.ObjectId, refPath: "types" },
   types: { type: String, enum: ["Product", "Category"] },
-});
+})
+  .pre("save", (next) => {
+    console.log("New like coming in");
+    next();
+  })
+  .post("save", (doc) => {
+    console.log("Like is saved");
+    console.log(doc);
+  })
+  .pre("find", (next) => {
+    console.log("Retrieving likes");
+    next();
+  })
+  .post("find", (doc) => {
+    console.log("Find is completed");
+    console.log(doc);
+  });
